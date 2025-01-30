deb2ee59c0602822419b906152481403f4819460
John Pontes <jpontes@questrade.com>
2025-01-30 08:04:56 -0300

    Types in functions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-class Book {
-  constructor (public title: string) {}
+// class Book {
+//   constructor (public title: string) {}
+// }
+
+// let book: Book;
+// book = new Book('Clean Code');
+
+function init (port: number) {
+  console.log(port);
 }
 
-let book: Book;
-book = new Book('Clean Code');
+init(80);
+
+function add (a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(1, 2));
