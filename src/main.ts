8000dbf541189c693d4cdf78b0d8bb1a548a619b
John Pontes <jpontes@questrade.com>
2025-01-30 08:18:29 -0300

    Types in Class

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,26 @@
 // let book: Book;
 // book = new Book('Clean Code');
 
-function init (port: number) {
-  console.log(port);
+// function init (port: number) {
+//   console.log(port);
+// }
+
+// init(80);
+
+// function add (a: number, b: number): number {
+//   return a + b;
+// }
+
+// console.log(add(1, 2));
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor (name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 }
-
-init(80);
-
-function add (a: number, b: number): number {
-  return a + b;
-}
-
-console.log(add(1, 2));
+const person = new Person("John Connor", 30);
+console.log(person);
