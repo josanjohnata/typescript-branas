1cd5e0c138b7279ebb745f3630a462f00fcbd27e
John Pontes <jpontes@questrade.com>
2025-01-31 22:46:54 -0300

    Types in Arrays

--- src/main.ts
+++ src/main.ts
@@ -17,14 +17,31 @@
 
 // console.log(add(1, 2));
 
+// class Person {
+//   name: string;
+//   age: number;
+
+//   constructor (name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+// const person = new Person("John Connor", 30);
+// console.log(person);
+
+const numbers: number[] = [];
+numbers[0] = 10;
+numbers[1] = 11;
+numbers[2] = 12;
+console.log(numbers);
 class Person {
   name: string;
-  age: number;
 
-  constructor (name: string, age: number) {
+  constructor (name: string) {
     this.name = name;
-    this.age = age;
   }
 }
-const person = new Person("John Connor", 30);
-console.log(person);
+
+const people: Person[] = [];
+people[0] = new Person('John');
+console.log(people); 
