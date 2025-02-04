62cb25cc15276787766ec094a5f5cea269aa8642
John Pontes <jpontes@questrade.com>
2025-02-03 21:16:00 -0300

    Types and Interface

--- src/main.ts
+++ src/main.ts
@@ -29,19 +29,37 @@
 // const person = new Person("John Connor", 30);
 // console.log(person);
 
-const numbers: number[] = [];
-numbers[0] = 10;
-numbers[1] = 11;
-numbers[2] = 12;
-console.log(numbers);
-class Person {
-  name: string;
+// const numbers: number[] = [];
+// numbers[0] = 10;
+// numbers[1] = 11;
+// numbers[2] = 12;
+// console.log(numbers);
+// class Person {
+//   name: string;
 
-  constructor (name: string) {
-    this.name = name;
-  }
+//   constructor (name: string) {
+//     this.name = name;
+//   }
+// }
+
+// const people: Person[] = [];
+// people[0] = new Person('John');
+// console.log(people); 
+
+interface Entity {
+  id: string;
 }
 
-const people: Person[] = [];
-people[0] = new Person('John');
-console.log(people); 
+interface Person {
+  name: string;
+  age: number;
+  paradigm: string;
+}
+
+type PersonEntity = Person & Entity;
+
+const people: PersonEntity[] = [];
+people[0] = { id: 'x1', name: 'John', age: 30, paradigm: 'OOP' };
+people[1] = { id: 'x2', name: 'Doe', age: 30, paradigm: 'FP' };
+
+console.log(people);
